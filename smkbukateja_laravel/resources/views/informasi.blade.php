2ba6c101ed2c8a06a99c17e3a8aa542790df303a
Rifqi Charisma <rifqibarker@gmail.com>
2022-01-14 14:01:56 +0700

    update navbar

--- smkbukateja_laravel/resources/views/informasi.blade.php
+++ smkbukateja_laravel/resources/views/informasi.blade.php
@@ -67,61 +67,7 @@
 
 <body style="background-color:#EEEEEE">
 	<!-- NAVBAR -->
-	<nav class="navbar navbar-expand-sm">
-		<div class="container">
-			<div class="skansika">
-				<img src="img/Logo SMK.png" alt="" class="logo">
-				<div class="name-skansika">
-					SKANSIKA
-				</div>
-			</div>
-
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-				aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav ms-auto">
-					<li class="beranda"><a href="/">Beranda</a></li>
-					<li class="profil-Sekolah"><a href="profil">Profil Sekolah</a></li>
-					<!-- dropdown -->
-					<li class="program-Keahlian dropdown">
-						<a href="" class="dropdown-toggle" href="#" id="navbarDropdown" role="button"
-							data-bs-toggle="dropdown" aria-expanded="false">
-							Program Keahlian
-						</a>
-						<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<li><a class="dropdown-item" href="jurusan/DPIB.html">Desain Permodelan Bangunan</a></li>
-                            <li><a class="dropdown-item" href="jurusan/TB.html">Tata Busana</a></li>
-                            <li><a class="dropdown-item" href="jurusan/TKRO.html">TKR</a></li>
-                            <li><a class="dropdown-item" href="jurusan/TKJ.html">TKJ</a></li>
-                            <li><a class="dropdown-item" href="jurusan/Multimedia.html">Multimedia</a></li>
-                            <li><a class="dropdown-item" href="jurusan/RPL.html">RPL</a></li>
-                            <li><a class="dropdown-item" href="jurusan/TBO.html">Teknik Body Otomotif</a></li>
-						</ul>
-					</li>
-					<li class="guru-karyawan"><a href="">Guru dan Karyawan</a></li>
-					<li class="prestasi"><a href="prestasi">Prestasi</a></li>
-					<!-- dropdown navbar -->
-					<li class="informasi dropdown">
-						<a href="" class="dropdown-toggle" href="#" id="navbarDropdown" role="button"
-							data-bs-toggle="dropdown" aria-expanded="false">
-							Informasi
-						</a>
-						<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<li><a class="dropdown-item" href="informasi">Berita</a></li>
-							<li><a class="dropdown-item" href="#">Kegiatan Intra Sekolah</a></li>
-							<li><a class="dropdown-item" href="#">Kegiatan Ekstra Sekolah</a></li>
-							<li><a class="dropdown-item" href="#">SEMAKIN</a></li>
-							<li><a class="dropdown-item" href="#">LSP</a></li>
-							<li><a class="dropdown-item" href="#">SIPANDIK</a></li>
-							<li><a class="dropdown-item" href="#">E-Learning</a></li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+	@include('navbar')
 	<!-- NAVBAR -->
 	<br>
 	<!-- BODY JUDUL -->
