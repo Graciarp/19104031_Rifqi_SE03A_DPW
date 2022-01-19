b6b12f9d1618df14d3ba25dfa4e099662239cf12
Graciarp <graciariska1262@gmail.com>
2022-01-19 13:12:35 +0700

    Membuat migration guru dan karyawan

--- smkbukateja_laravel/resources/views/layouts/app.blade.php
+++ smkbukateja_laravel/resources/views/layouts/app.blade.php
@@ -64,6 +64,11 @@
                                 </a>
                             </li>
                             <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    Guru dan Karyawan
+                                </a>
+                            </li>
+                            <li class="nav-item">
                                 <a id="navbarDropdown" class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
