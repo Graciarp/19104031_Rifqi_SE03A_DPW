21f669e55c77183ff9e311c17b77d9a32128f280
Gracia Rizka Pasfica <graciariska1262@gmail.com>
2022-01-31 11:46:45 +0700

    Bug fix

--- smkbukateja_laravel/resources/views/prestasi.blade.php
+++ smkbukateja_laravel/resources/views/prestasi.blade.php
@@ -12,6 +12,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
+    @include('layouts.meta_seo')
+
     {{-- Import All Css --}}
     @include('layouts.css')
 
